148e005f94d725396a46040423d7cb3b1438117d
styv <styv2301@gmail.com>
2026-03-19 09:24:04 -0500

    refactor: se cambio en nombre para el manifest generado a Ahiseve Extension

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   // modules: ['@wxt-dev/module-react'],
   manifest: {
+    name: 'Ahiseve Extension',
     permissions: ['tabs', 'scripting', 'webNavigation', 'storage'],
     host_permissions: ['*://*/*'],
   },
